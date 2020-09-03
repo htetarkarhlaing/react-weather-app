79e393b6d73622f1f6cf3528b9d9f265b4837108
htetarkarhlaing <htet.arkar.hlaing.official.mm@gmail.com>
2020-09-03 15:10:33 +0630

    Update : Inserting the new gh-pages features

---
+++ build/precache-manifest.e9730298e8862ff228fbaf4f66671bac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40ecf657be88a6cb1d781ee036b8d9a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1536d0bcd7d3234310a",
+    "url": "/static/css/main.c9e81d04.chunk.css"
+  },
+  {
+    "revision": "d661b0d815234a5401ea",
+    "url": "/static/js/2.44998ee1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.44998ee1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1536d0bcd7d3234310a",
+    "url": "/static/js/main.1eb2c87d.chunk.js"
+  },
+  {
+    "revision": "3b906a68717433f63125",
+    "url": "/static/js/runtime-main.33336675.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
